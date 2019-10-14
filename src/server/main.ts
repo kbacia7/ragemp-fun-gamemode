7a97c4989b9d3adf1f071c3ff928d7846ee01472
Kamil <kbacia7@gmail.com>
2019-10-14 22:55:36 +0200

    Instalacja knex i objection.js, konfiguracja tsconfig

--- src/server/main.ts
+++ src/server/main.ts
@@ -3,16 +3,19 @@ import { XMLFileRequest } from "core/FileRequest/XMLFileRequest"
 import { I18nTranslate } from "core/i18n/I18nTranslate"
 import { InternationalizationSettings } from "core/i18n/InternationalizationSettings"
 import { PlayerDataFactory } from "core/PlayerDataProps/PlayerDataFactory"
+import Knex from "knex"
 import _ from "lodash"
-import { PlayerLoader } from "./modules/PlayerLoader/PlayerLoader"
-mp.events.add("debug", (player: PlayerMp, text: string) => {
-   /*tslint:disable:no-console*/
-   console.log(text)
-})
+import dbConfig from "./knexfile"
 
+import { PlayerLoader } from "./modules/PlayerLoader/PlayerLoader"
 const playerDataFactory = new PlayerDataFactory()
 const activePlayers: ActivePlayers = new ActivePlayers(playerDataFactory)
 const playerLoader: PlayerLoader = new PlayerLoader(playerDataFactory)
+
+const knex = Knex({
+   client: dbConfig.development.client,
+   connection: dbConfig.development.connection,
+})
 
 mp.events.addCommand("hp", (player: PlayerMp) => {
    player.health = 100
@@ -20,4 +23,8 @@ mp.events.addCommand("hp", (player: PlayerMp) => {
 
 mp.events.addCommand("armor", (player: PlayerMp) => {
    player.armour = 100
+})
+
+mp.events.add("debug", (player: PlayerMp, text: string) => {
+   console.log(text)
 })
