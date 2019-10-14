f8f0202f8e67dcc7cae9667cc5469919dea5b7c7
Kamil <kbacia7@gmail.com>
2019-10-15 01:31:27 +0200

    #8, Przykładowe powiadomienie dla czatu

--- src/server/modules/Chat/Chat.ts
+++ src/server/modules/Chat/Chat.ts
@@ -1,7 +1,10 @@
+import { NotificationTimeout } from "core/Notification/NotificationTimeout"
+import { NotificationType } from "core/Notification/NotificationType"
 import { PlayerDataProps } from "core/PlayerDataProps/PlayerDataProps"
+import { INotificationSender } from "server/core/NotificationSender/INotificationSender"
 
 export class Chat {
-    constructor() {
+    constructor(notificationSender: INotificationSender) {
         mp.events.add("playerChat", (player: PlayerMp, message: string) => {
             const senderColor: string = player.getVariable(PlayerDataProps.NAMECOLOR)
             const senderName: string = player.getVariable(PlayerDataProps.NAME)
@@ -9,6 +12,11 @@ export class Chat {
                 mp.players.forEach((sendMessageTo: PlayerMp) => {
                     sendMessageTo.outputChatBox(`!{${senderColor}}${senderName}!{#FFFFFF}: ${message}`)
                 })
+            } else {
+                notificationSender.send(
+                    player, "NOTIFICATION_CHAT_INCORRECT_VALUE",
+                    NotificationType.ERROR, NotificationTimeout.NORMAL,
+                )
             }
         })
     }
