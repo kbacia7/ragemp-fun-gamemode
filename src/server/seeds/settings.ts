568a9bb42fe296e36bd4c050ddeb62260b2d426b
Kamil <kbacia7@gmail.com>
2019-10-25 02:44:13 +0200

    #36, Dodatkowe ustawienia do bazy danych dla race

--- src/server/seeds/settings.ts
+++ src/server/seeds/settings.ts
@@ -18,6 +18,36 @@ export async function seed(knex: Knex): Promise<any> {
                     name: "race_max_players",
                     value: "30",
                 },
+                {
+                    description: "Minimalna kwota jaką może otrzymać gracz za wygraną na Race",
+                    id: 3,
+                    name: "race_min_money",
+                    value: "1000",
+                },
+                {
+                    description: "Maksymalna kwota jaką może otrzymać gracz za wygraną na Race",
+                    id: 4,
+                    name: "race_max_money",
+                    value: "3000",
+                },
+                {
+                    description: "Minimalna liczba expa jaką może otrzymać gracz za wygraną na Race",
+                    id: 5,
+                    name: "race_min_exp",
+                    value: "3",
+                },
+                {
+                    description: "Maksymalna liczba expa jaką może otrzymać gracz za wygraną na Race",
+                    id: 6,
+                    name: "race_max_exp",
+                    value: "10",
+                },
+                {
+                    description: "Nazwa wyświetlana dla Race w tabeli",
+                    id: 7,
+                    name: "race_display_name",
+                    value: "Race",
+                },
 
             ])
         })
