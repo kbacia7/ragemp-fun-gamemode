9f4de315d806a2c0093fa86ab7b9ba8dfd91cafa
Kamil <kbacia7@gmail.com>
2019-10-29 23:04:51 +0100

    #36, Klasa dla fabryki eventów

--- src/server/modules/AutomaticEvents/IAutomaticEventFactory.ts
+++ src/server/modules/AutomaticEvents/IAutomaticEventFactory.ts
@@ -1,6 +1,7 @@
+import { AutomaticEvent } from "./AutomaticEvent"
 import { IAutomaticEvent } from "./IAutomaticEvent"
 import { IAutomaticEventData } from "./IAutomaticEventData"
 
 export interface IAutomaticEventFactory {
-    create: (automaticEventData: IAutomaticEventData) => IAutomaticEvent
+    create: (automaticEventData: IAutomaticEventData) => AutomaticEvent
 }
