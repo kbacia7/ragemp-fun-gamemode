8f2e1cc4f0415cb6bc9b10b9892e99fac2df9019
Kamil <kbacia7@gmail.com>
2019-11-07 18:57:30 +0100

    clsoe #75, Wysyłanie odpowiednich eventów gdy gracz wyjdzie w czasie eventu/gdy jest zapisany

---
+++ src/server/modules/PlayerSave/PlayerQuitEvents.ts
@@ -0,0 +1,4 @@
+export enum PlayerQuitEvents {
+    PLAYER_QUIT_ON_EVENT = "playerQuitWhenIsOnEvent",
+    PLAYER_QUIT_SAVED_ON_EVENT = "playerQuitWhevSavedOnEvent",
+}
--- src/server/modules/PlayerSave/PlayerSave.ts
+++ src/server/modules/PlayerSave/PlayerSave.ts
@@ -1,9 +1,11 @@
 import { IPlayerData } from "core/PlayerDataProps/IPlayerData"
 import { IPlayerDataFactory } from "core/PlayerDataProps/IPlayerDataFactory"
 import { PlayerDataProps } from "core/PlayerDataProps/PlayerDataProps"
+import { PlayerDataStatus } from "core/PlayerDataProps/PlayerDataStatus"
 import { PlayerRegisterEvent } from "core/PlayerRegister/PlayerRegisterEvent"
 import Knex from "knex"
 import { Player } from "server/entity/Player"
+import { PlayerQuitEvents } from "./PlayerQuitEvents"
 
 export class PlayerSave {
     constructor(knex: Knex, playerDataFactory: IPlayerDataFactory) {
@@ -18,6 +20,11 @@ export class PlayerSave {
                 })
                 .where("login", "LIKE", playerData.name)
                 .execute()
+            }
+            if (playerData.status === PlayerDataStatus.ON_EVENT) {
+                mp.events.call(PlayerQuitEvents.PLAYER_QUIT_ON_EVENT, playerData)
+            } else if (playerData.savedOnEvents.length > 0) {
+                mp.events.call(PlayerQuitEvents.PLAYER_QUIT_SAVED_ON_EVENT, playerData)
             }
         })
     }
