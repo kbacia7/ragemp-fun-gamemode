aaa943baf7a56a86c3030403cf144de3990a4f39
Kamil <kbacia7@gmail.com>
2019-11-09 02:14:07 +0100

    #37, Rozbudowany seed o ustawienia dla TDM

--- src/server/seeds/settings.ts
+++ src/server/seeds/settings.ts
@@ -49,6 +49,49 @@ export async function seed(knex: Knex): Promise<any> {
                     value: "Race",
                 },
 
+                {
+                    description: "Minimalna liczba graczy do startu zabawy TDM",
+                    id: 8,
+                    name: "tdm_min_players",
+                    value: "1",
+                },
+                {
+                    description: "Maksymalna liczba graczy jaka może się zapisać na TDM",
+                    id: 9,
+                    name: "tdm_max_players",
+                    value: "30",
+                },
+                {
+                    description: "Minimalna kwota jaką może otrzymać gracz za wygraną na TDM",
+                    id: 10,
+                    name: "tdm_min_money",
+                    value: "1000",
+                },
+                {
+                    description: "Maksymalna kwota jaką może otrzymać gracz za wygraną na TDM",
+                    id: 11,
+                    name: "tdm_max_money",
+                    value: "3000",
+                },
+                {
+                    description: "Minimalna liczba expa jaką może otrzymać gracz za wygraną na TDM",
+                    id: 12,
+                    name: "tdm_min_exp",
+                    value: "3",
+                },
+                {
+                    description: "Maksymalna liczba expa jaką może otrzymać gracz za wygraną na TDM",
+                    id: 13,
+                    name: "tdm_max_exp",
+                    value: "10",
+                },
+                {
+                    description: "Nazwa wyświetlana dla tdm w tabeli",
+                    id: 14,
+                    name: "tdm_display_name",
+                    value: "TDM",
+                },
+
             ])
         })
 }
