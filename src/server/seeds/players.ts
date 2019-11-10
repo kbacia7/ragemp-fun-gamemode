e8fb8dd8671d6b6193f2ea1bd11414e08d7d1b66
Kamil <kbacia7@gmail.com>
2019-11-10 01:10:12 +0100

    Drobna aktualizacja w seedzie

--- src/server/seeds/players.ts
+++ src/server/seeds/players.ts
@@ -3,10 +3,18 @@ import * as Knex from "knex"
 export async function seed(knex: Knex): Promise<any> {
     return knex("players").del()
         .then(() => {
+            // tslint:disable: object-literal-sort-keys
             return knex("players").insert([
-                { id: 1, login: "testPlayer1", password: "123", rank: "Player", deaths: 0, kills: 0},
-                { id: 2, login: "testPlayer2", password: "123", rank: "Player", deaths: 20, kills: 30, ped: 0xD1FEB884},
-                { id: 3, login: "testPlayer3", password: "123", rank: "AdminOrSomething", deaths: 10, kills: 10},
+                {
+                    email: "t@mail.com",
+                    id: 1,
+                    login: "testPlayer1",
+                    password: "123",
+                    rank: "Player",
+                    deaths: 0,
+                    kills: 0,
+                    ped: 0xD1FEB884,
+                },
 
             ])
         })
