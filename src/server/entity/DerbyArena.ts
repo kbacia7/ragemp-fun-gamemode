c3020f42e8ee48556dab998576c21c267053d5f8
Kamil <kbacia7@gmail.com>
2019-11-24 14:17:11 +0100

    #38, Modele dla aren derby i spawnów

---
+++ src/server/entity/DerbyArena.ts
@@ -0,0 +1,24 @@
+import { Model } from "objection"
+import { DerbyArenaSpawnPoint } from "./DerbyArenaSpawnPoint"
+
+export class DerbyArena extends Model {
+  public static tableName = "derby_arenas"
+  public readonly id!: number
+  public name: string
+  public author: string
+  public vehicleModel: number
+
+  public static get relationMappings()  {
+    return {
+      spawns: {
+        join: {
+          from: "derby_arenas.id",
+          to: "derby_arenas_spawns.arenaId",
+        },
+        modelClass: DerbyArenaSpawnPoint,
+        relation: Model.HasManyRelation,
+      },
+    }
+
+}
+}
---
+++ src/server/entity/DerbyArenaSpawnPoint.ts
@@ -0,0 +1,24 @@
+import { Model } from "objection"
+import { DerbyArena } from "./RaceArena"
+
+export class DerbyArenaSpawnPoint extends Model {
+  public static tableName = "derby_arenas_spawns"
+
+  public static relationMappings = {
+    arena: {
+      join: {
+        from: "derby_arenas_spawns.arenaId",
+        to: "derby_arenas.id",
+      },
+      modelClass: DerbyArena,
+      relation: Model.BelongsToOneRelation,
+    },
+
+}
+  public readonly id!: number
+  public arenaId: number
+  public x: number
+  public y: number
+  public z: number
+  public rotation: number
+}
