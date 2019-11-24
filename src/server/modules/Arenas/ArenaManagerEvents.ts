9a18f3ee4f0ead8072a5a5cd2faf253783ea6093
Kamil <kbacia7@gmail.com>
2019-11-24 21:13:22 +0100

    #41, Klasa do obsługi wszystkich aren z eventami

---
+++ src/server/modules/Arenas/ArenaManagerEvents.ts
@@ -0,0 +1,8 @@
+export enum ArenaManagerEvents {
+    PLAYER_JOIN_ARENA = "arenas-manager-join-arena",
+    UPDATE_ARENAS_TABLE = "arenas-manager-table-update",
+    PROVIDE_ARENAS = "arenas-manager-provide-events",
+    GET_ARENAS = "arenas-manager-get-events",
+    PLAYER_QUIT_ARENA = "arenas-manager-quit-arena",
+    UPDATE_ARENAS_BUTTON_TABLE = "arenas-manager-button-update",
+}
