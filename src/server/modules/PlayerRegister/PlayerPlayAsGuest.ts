e6bb34ebf46b909e591c3567a1334297f9f292d2
kerorero <kerorero13>
2020-03-15 13:32:19 +0100

    #88, Przeniesione granie jako gość na REST API

--- src/server/modules/PlayerRegister/PlayerPlayAsGuest.ts
+++ src/server/modules/PlayerRegister/PlayerPlayAsGuest.ts
@@ -7,6 +7,9 @@ import { PlayerRegisterEvent } from "core/PlayerRegister/PlayerRegisterEvent"
 import { IPromiseFactory } from "core/PromiseFactory/IPromiseFactory"
 import { Player } from "server/entity/Player"
 import { IAPIManager } from "server/core/API/IAPIManager"
+import { APIRequests } from "server/core/API/APIRequests"
+import { IncomingMessage } from "http"
+import { PlayerPlayAsGuestResponses } from "server/core/API/PlayerPlayAsGuestResponses"
 
 export class PlayerPlayAsGuest {
     private _playerLoginValidatorFactory: IPlayerLoginValidatorFactory = null
@@ -21,18 +24,29 @@ export class PlayerPlayAsGuest {
             if (!this._playerLoginValidatorFactory.create().validate(login)) {
                 player.call(PlayerRegisterEvent.UNKNOWN_ERROR)
             }  else {
-                /*Player.query()
-                    .select("login")
-                    .where("login", "LIKE", login)
-                    .then((players: Player[]) => {
-                        if (players.length <= 0) {
-                            player.call(PlayerRegisterEvent.PLAY_AS_GUEST_SUCCESS)
-                            player.setVariable(PlayerDataProps.PLAY_AS_GUEST, true)
-                            mp.events.call("playerStartPlay", player, login)
-                        } else {
-                            player.call(PlayerRegisterEvent.LOGIN_TAKEN_FOR_GUEST)
+                apiManager.send(APIRequests.PLAY_AS_GUEST, {
+                    login: login
+                }).then((res: IncomingMessage) => {
+                    let responseAsString: string = ""
+                    res.on('data', (chunk) => {
+                        responseAsString += chunk
+                    })
+                    res.on('end', () => {
+                        const response: number = parseInt(responseAsString)
+                        switch(response) {
+                            case PlayerPlayAsGuestResponses.ALL_OK: {
+                                player.call(PlayerRegisterEvent.PLAY_AS_GUEST_SUCCESS)
+                                player.setVariable(PlayerDataProps.PLAY_AS_GUEST, true)
+                                mp.events.call("playerStartPlay", player, login)
+                                break
+                            }
+                            case PlayerPlayAsGuestResponses.LOGIN_TAKEN: {
+                                player.call(PlayerRegisterEvent.LOGIN_TAKEN_FOR_GUEST)
+                                break
+                            }
                         }
-                    })*/
+                    })
+                })
             }
         })
     }
