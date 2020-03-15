23c3937ded13412ecca035c8e171b08fefb4ed34
kerorero <kerorero13>
2020-03-15 13:35:14 +0100

    #88, Możliwość wysyłania POST po API,  zwracanie odpowiedniego obiektu

--- src/server/core/API/APIManager.ts
+++ src/server/core/API/APIManager.ts
@@ -1,12 +1,20 @@
 import { IPromiseFactory } from "core/PromiseFactory/IPromiseFactory"
-import http from "http"
+import http, { IncomingMessage } from "http"
 import { IAPIManager } from "./IAPIManager"
 import { IAPISetting } from "./IAPISetting"
+import querystring from "querystring"
+
 export class APIManager<T> implements IAPIManager<T> {
     private _apiSetting: IAPISetting = null
     private _promiseFactory: IPromiseFactory<T[]> = null
-    constructor(promiseFactory: IPromiseFactory<T[]>, apiSetting: IAPISetting) {
+    private _promiseFactoryForPosts: IPromiseFactory<IncomingMessage> = null
+    constructor(
+        promiseFactory: IPromiseFactory<T[]>, 
+        promiseFactoryForPosts: IPromiseFactory<IncomingMessage>,
+        apiSetting: IAPISetting
+    ) {
         this._promiseFactory = promiseFactory
+        this._promiseFactoryForPosts = promiseFactoryForPosts
         this._apiSetting = apiSetting
     }
     public query(path: string) {
@@ -28,6 +36,28 @@ export class APIManager<T> implements IAPIManager<T> {
             })
             r.end()
         })
-
+    }
+    public send(path: string, data: any) {
+        return this._promiseFactoryForPosts.create((resolve) => {
+            const dataString = querystring.stringify(data)
+            const options = {
+                hostname: this._apiSetting.host,
+                port: this._apiSetting.port,
+                path: path,
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded',
+                    'Content-Length': Buffer.byteLength(dataString)
+                }
+            }
+            const r = http.request(options, (res: IncomingMessage) => {
+                resolve(res)
+            })
+            r.on('error', (d) => {
+                console.log(d)
+            })
+            r.write(dataString)    
+            r.end()
+        })
     }
 }
