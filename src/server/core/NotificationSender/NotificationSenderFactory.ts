2de609cab86c0ba5cf71a65dbb3f6b2bc3afff47
kerorero <kerorero13@gmail.com>
2020-03-18 22:40:25 +0100

    #96, Wysyłanie powiadomień na czat Powiadomienia

--- src/server/core/NotificationSender/NotificationSenderFactory.ts
+++ src/server/core/NotificationSender/NotificationSenderFactory.ts
@@ -1,12 +1,17 @@
 import { NotificationEvent } from "core/Notification/NotificationEvent"
 import { NotificationTimeout } from "core/Notification/NotificationTimeout"
 import { NotificationType } from "core/Notification/NotificationType"
+import { IChatSpecialTabSender } from "server/modules/Chat/Senders/IChatSpecialTabSender"
 import { INotificationSender } from "./INotificationSender"
 import { INotificationSenderFactory } from "./INotificationSenderFactory"
 import { NotificationSender } from "./NotificationSender"
 
 export class NotificationSenderFactory implements INotificationSenderFactory {
+    private _notificationTabSender: IChatSpecialTabSender = null
+    constructor(notificationTabSender: IChatSpecialTabSender) {
+        this._notificationTabSender = notificationTabSender
+    }
     public create() {
-        return new NotificationSender()
+        return new NotificationSender(this._notificationTabSender)
     }
 }
