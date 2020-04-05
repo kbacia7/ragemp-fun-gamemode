8191490034f0284e91c0348d726d747947426fcd
kerorero <kerorero13@gmail.com>
2020-04-05 04:50:04 +0200

    #108, Spawn broni co spawn (jeśli założona)

--- src/server/modules/PlayerSpawnManager/PlayerSpawnManager.ts
+++ src/server/modules/PlayerSpawnManager/PlayerSpawnManager.ts
@@ -1,4 +1,5 @@
 import { ChangePlayerPedModuleEvents } from "client/modules/ChangePlayerPedModule/ChangePlayerPedModuleEvents"
+import { ItemsSectionsNames } from "core/ItemsSectionsNames/ItemsSectionsNames"
 import { IPlayerDataFactory } from "core/PlayerDataProps/IPlayerDataFactory"
 import { PlayerDataStatus } from "core/PlayerDataProps/PlayerDataStatus"
 import random from "random"
@@ -6,6 +7,7 @@ import { APIRequests } from "server/core/API/APIRequests"
 import { IAPIManager } from "server/core/API/IAPIManager"
 import { Dimension } from "server/core/Dimension/Dimension"
 import { IVector3Factory } from "server/core/Vector3Factory/IVector3Factory"
+import { PlayerItem } from "server/entity/PlayerItem"
 import { PlayerSpawn } from "server/entity/PlayerSpawn"
 import { PlayerSpawnManagerEvents } from "./PlayerSpawnManagerEvents"
 
@@ -29,6 +31,7 @@ export class PlayerSpawnManager {
 
         mp.events.add(PlayerSpawnManagerEvents.FORCE_RESPAWN, (playerMp: PlayerMp) => {
             const playerData = playerDataFactory.create().load(playerMp)
+            playerMp.removeAllWeapons()
             const spawnId = random.int(0, this._spawns.length - 1)
             playerMp.dimension = Dimension.NORMAL
             playerMp.spawn(
@@ -38,8 +41,15 @@ export class PlayerSpawnManager {
                     this._spawns[spawnId].z,
                 ),
             )
-            playerMp.removeAllWeapons()
+            const weaponsOnSpawn: number[] = playerData.items.filter(
+                (playerItem: PlayerItem) =>
+                    playerItem.item.section.name === ItemsSectionsNames.WEAPON && playerItem.equipped,
+            ).map((playerItem) => playerItem.item.ragemp_item_id)
+
             playerMp.call(ChangePlayerPedModuleEvents.CHANGE_PED, [playerData.ped])
+            weaponsOnSpawn.forEach((weaponId) => {
+                playerMp.giveWeapon(weaponId, 99999999)
+            })
         })
     }
 }
