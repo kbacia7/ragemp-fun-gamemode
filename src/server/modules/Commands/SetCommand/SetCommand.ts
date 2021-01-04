7fc985ca6999f7bc6526dccdcb45422f0dd89b0b
Kamil Bacia <kbacia7@gmail.com>
2021-01-04 20:44:26 +0100

    Drobny dodatek, by móc ustawiać swoją pozycję

--- src/server/modules/Commands/SetCommand/SetCommand.ts
+++ src/server/modules/Commands/SetCommand/SetCommand.ts
@@ -2,6 +2,7 @@ import { CommandListenerEvent } from "core/CommandListener/CommandListenerEvent"
 import { IPlayerData } from "core/PlayerDataProps/IPlayerData"
 import { IPlayerDataFactory } from "core/PlayerDataProps/IPlayerDataFactory"
 import { PlayerDataProps } from "core/PlayerDataProps/PlayerDataProps"
+import { Vector3Factory } from "server/core/Vector3Factory/Vector3Factory"
 import { ICommand } from "../ICommand"
 
 export class SetCommand implements ICommand {
@@ -19,6 +20,10 @@ export class SetCommand implements ICommand {
             player.armour = parseInt(args[1], 10)
         } else if (args[0] === "deaths") {
             player.setVariable(PlayerDataProps.KILLS, parseInt(args[1], 10))
+        } else if (args[0] === "pos") {
+            player.spawn(new Vector3Factory().create(
+                parseFloat(args[1]), parseFloat(args[2]), parseFloat(args[3]),
+            ))
         }
     }
 }
